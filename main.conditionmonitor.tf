e5a2f38c32186c5438fd11e532f3571f45cd2047
Terry Mandin <terry.mandin@microsoft.com>
2024-11-18 14:14:20 -0700

    Linting fixes

--- main.conditionmonitor.tf
+++ main.conditionmonitor.tf
@@ -10,6 +10,7 @@ resource "azurerm_network_connection_monitor" "this" {
 
   dynamic "endpoint" {
     for_each = each.value.endpoint == null ? [] : each.value.endpoint
+
     content {
       name                  = endpoint.value.name
       address               = endpoint.value.address
@@ -21,11 +22,13 @@ resource "azurerm_network_connection_monitor" "this" {
 
       dynamic "filter" {
         for_each = endpoint.value.filter == null ? [] : [endpoint.value.filter]
+
         content {
           type = filter.value.type
 
           dynamic "item" {
             for_each = filter.value.item == null ? [] : filter.value.item
+
             content {
               address = item.value.address
               type    = item.value.type
@@ -37,6 +40,7 @@ resource "azurerm_network_connection_monitor" "this" {
   }
   dynamic "test_configuration" {
     for_each = each.value.test_configuration == null ? [] : each.value.test_configuration
+
     content {
       name                      = test_configuration.value.name
       protocol                  = test_configuration.value.protocol
@@ -45,6 +49,7 @@ resource "azurerm_network_connection_monitor" "this" {
 
       dynamic "http_configuration" {
         for_each = test_configuration.value.http_configuration == null ? [] : [test_configuration.value.http_configuration]
+
         content {
           method                   = http_configuration.value.method
           path                     = http_configuration.value.path
@@ -54,6 +59,7 @@ resource "azurerm_network_connection_monitor" "this" {
 
           dynamic "request_header" {
             for_each = http_configuration.value.request_header == null ? [] : http_configuration.value.request_header
+
             content {
               name  = request_header.value.name
               value = request_header.value.value
@@ -63,12 +69,14 @@ resource "azurerm_network_connection_monitor" "this" {
       }
       dynamic "icmp_configuration" {
         for_each = test_configuration.value.icmp_configuration == null ? [] : [test_configuration.value.icmp_configuration]
+
         content {
           trace_route_enabled = icmp_configuration.value.trace_route_enabled
         }
       }
       dynamic "success_threshold" {
         for_each = test_configuration.value.success_threshold == null ? [] : [test_configuration.value.success_threshold]
+
         content {
           checks_failed_percent = success_threshold.value.checks_failed_percent
           round_trip_time_ms    = success_threshold.value.round_trip_time_ms
@@ -76,6 +84,7 @@ resource "azurerm_network_connection_monitor" "this" {
       }
       dynamic "tcp_configuration" {
         for_each = test_configuration.value.tcp_configuration == null ? [] : [test_configuration.value.tcp_configuration]
+
         content {
           port                      = tcp_configuration.value.port
           destination_port_behavior = tcp_configuration.value.destination_port_behavior
@@ -86,6 +95,7 @@ resource "azurerm_network_connection_monitor" "this" {
   }
   dynamic "test_group" {
     for_each = each.value.test_group == null ? [] : each.value.test_group
+
     content {
       destination_endpoints    = test_group.value.destination_endpoints
       name                     = test_group.value.name
